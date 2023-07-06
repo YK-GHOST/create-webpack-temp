ed74c3d01dda471ac06fadbb0a16ada2fb2ba33b
YK-GHOST <yogeshkantiwal20@gmail.com>
2023-07-06 16:15:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('template', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Metadata description.',
+      },
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ readme.txt
@@ -0,0 +1,4 @@
+1. run command to install all packages -->
+    npm install
+
+2. delete the cool.txt file form /shared (just a testing file for CleanWebpackPlugin)
